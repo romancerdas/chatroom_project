7f37199e8f2a6be43108ccf39861a1d0c6a49083
romancerdas <159666844+romancerdas@users.noreply.github.com>
2026-04-28 19:44:50 -0700

    added comments that explains what code does

--- client_cli.py
+++ client_cli.py
@@ -1,26 +1,26 @@
 import socket
 from protocol import encode_message
 
-HOST = "127.0.0.1"
-PORT = 5000
+HOST = "127.0.0.1" # Set IP address     # for now, HOST will be set as the localhost IP, will be updated later in development 
+PORT = 5000 # Set Port number     # same as IP, set to 5000 for now, will be updated later in development 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a server side socket using IPv4 address family and TCP protocol 
+client.connect((HOST, PORT)) # connects to the client socket at HOST and PORT 
 
-username = input("Enter username: ")
+username = input("Enter username: ") # user inputs a username 
 
-while True:
-    text = input("Enter message, or type quit: ")
+while True: # message compiler loop
+    text = input("Enter message, or type quit: ") # user types a message or quits 
 
-    if text.lower() == "quit":
+    if text.lower() == "quit": # checks if user wants to quit 
         break
 
-    message = {
+    message = { # JSON composition of the message to be sent to server 
         "type": "SEND_MESSAGE",
         "username": username,
         "message": text
     }
 
-    client.send(encode_message(message))
+    client.send(encode_message(message)) # client sockets sends the encoded message to the server 
 
-client.close()
+client.close() # client socket closes 
--- server.py
+++ server.py
@@ -1,29 +1,29 @@
 import socket
 from protocol import decode_message
 
-HOST = "127.0.0.1"
-PORT = 5000
+HOST = "127.0.0.1" # Set IP address     # for now, HOST will be set as the localhost IP, will be updated later in development 
+PORT = 5000 # Set Port number     # same as IP, set to 5000 for now, will be updated later in development 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a server side socket using IPv4 address family and TCP protocol 
+server.bind((HOST, PORT)) # binds the server socket to the HOST and PORT 
+server.listen() # opens socket for connections 
 
-print(f"Server is listening on {HOST}:{PORT}")
+print(f"Server is listening on {HOST}:{PORT}") # Terminal message to states server state (listening)
 
-conn, addr = server.accept()
-print(f"Client connected from {addr}")
+conn, addr = server.accept() # instantiates conn socket object and address tuple when .accept() runs, which accepts an incoming connection
+print(f"Client connected from {addr}") # terminal message that states client connection and client address 
 
 while True:
-    data = conn.recv(1024)
+    data = conn.recv(1024) # assigns data variable to the bytes received from the client, with a 1024 byte buffer 
 
-    if not data:
+    if not data: # if no data is received, connection is broken 
         print("Client disconnected.")
         break
 
-    message = decode_message(data)
+    message = decode_message(data) # assigns message variable to the decoded data 
 
     print("Received message:")
-    print(message)
+    print(message) # prints message in terminal 
 
-conn.close()
-server.close()
+conn.close() # socket connection is closed 
+server.close() # server socket is closed 
