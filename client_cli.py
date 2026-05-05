d02f2ce08eec468c3488cc5dcf3a8502871ea107
romancerdas <159666844+romancerdas@users.noreply.github.com>
2026-05-05 14:13:12 -0700

    implemented multi-client support cli functionality

--- client_cli.py
+++ client_cli.py
@@ -1,5 +1,6 @@
 import socket
-from protocol import encode_message
+import threading
+from protocol import encode_message, decode_message 
 
 HOST = "127.0.0.1" # Set IP address     # for now, HOST will be set as the localhost IP, will be updated later in development 
 PORT = 5000 # Set Port number     # same as IP, set to 5000 for now, will be updated later in development 
@@ -8,6 +9,36 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a server si
 client.connect((HOST, PORT)) # connects to the client socket at HOST and PORT 
 
 username = input("Enter username: ") # user inputs a username 
+
+running = True # variable to control loops
+
+def receive_messages(): # defines how to handle incoming messages 
+
+    global running # allows function to modify the variable "running"
+
+    while running:
+        try:
+            data = client.recv(1024) # assigns incoming 1024 bytes to data variable 
+
+            if not data: # break connection if no data is received 
+                print("Disconnected from server.") # terminal message to state disconnection from server
+                running = False # sets "running" to false to break loop
+                break 
+
+            message = decode_message(data) # assigns message variable to the decoded data
+
+            print(f"\n [{message['username']}]: {message['message']}") # prints incoming message to terminal 
+            print("Enter message, or type 'quit': ", end="") # reprints message prompt after incoming message is printed
+
+        except: # error handling (i.e. disconnection)
+            break 
+
+
+
+thread = threading.Thread(target=receive_messages) # creates a new thread to run receive_messages()
+thread.daemon = True # sets thread as a daemon thread, meaning the process will end when the main thread ends 
+thread.start() # starts the thread 
+
 
 while True: # message compiler loop
     text = input("Enter message, or type quit: ") # user types a message or quits 
--- server.py
+++ server.py
@@ -1,8 +1,36 @@
 import socket
+import threading
 from protocol import decode_message
 
 HOST = "127.0.0.1" # Set IP address     # for now, HOST will be set as the localhost IP, will be updated later in development 
 PORT = 5000 # Set Port number     # same as IP, set to 5000 for now, will be updated later in development 
+
+clients = [] # an array to hold multiple clients
+
+def handle_client(conn, addr):
+    print(f"New Client has connected [{addr}]") # terminal message to states client connection and client address
+    clients.append(conn) # appends socket object to client array
+    while True:
+        try:
+            data = conn.recv(1024) # assigns incoming 1024 bytes to data variable 
+
+            if not data: # break connection if no data is received 
+                break
+
+            message = decode_message(data) # assigns message variable to the decoded data
+            print(f"Incoming message from {addr}: {message}") # print message and sender address in recipient terminal 
+
+            for client in clients: # send message to all clients in client array 
+                if client != conn: # if the client is not the sender, send the message to the client 
+                    client.send(data) # sends the data to the client
+
+        except: # error handling (i.e. disconnection)
+            break
+
+    print(f"Client has disconnected [{addr}]") # terminal message to states client disconnection and client address
+    if conn in clients: 
+        clients.remove(conn) # removes client socket object from client array
+    conn.close() # closes the client socket connection
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a server side socket using IPv4 address family and TCP protocol 
 server.bind((HOST, PORT)) # binds the server socket to the HOST and PORT 
@@ -10,20 +38,10 @@ server.listen() # opens socket for connections
 
 print(f"Server is listening on {HOST}:{PORT}") # Terminal message to states server state (listening)
 
-conn, addr = server.accept() # instantiates conn socket object and address tuple when .accept() runs, which accepts an incoming connection
-print(f"Client connected from {addr}") # terminal message that states client connection and client address 
+while True: 
+    conn, addr = server.accept() # instantiates conn socket object and address tuple when .accept() runs, which accepts an incoming connection
+    thread = threading.Thread(target=handle_client, args=(conn,addr)) # creates a new thread to run handle_client(), and passes conn and addr arguements 
+    thread.start() # starts the thread
 
-while True:
-    data = conn.recv(1024) # assigns data variable to the bytes received from the client, with a 1024 byte buffer 
-
-    if not data: # if no data is received, connection is broken 
-        print("Client disconnected.")
-        break
-
-    message = decode_message(data) # assigns message variable to the decoded data 
-
-    print("Received message:")
-    print(message) # prints message in terminal 
-
-conn.close() # socket connection is closed 
-server.close() # server socket is closed 
+    print(f"Active Connections: {threading.active_count() - 1}") # shows number of active connections, -1 to account for main thread 
+    
